d2b693c104f999249eed264c96d07f5065f8f76b
LemonNekoGH <LemonNekoGH@users.noreply.github.com>
2021-06-19 08:32:36 +0000

    deploy: dce8dd4f4793ba3144821859e759297a84b7db59

---
+++ _nuxt/static/1624091554/inspire/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624091554",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8ff0b8f3"}}}}(null));
