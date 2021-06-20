7bc19180ea6904f5bc5f76dafcdd600d94ab7839
LemonNeko <chheese048@gmail.com>
2021-06-20 15:17:15 +0800

    :sparkles: 更新一些样式
    Signed-off-by: LemonNeko <chheese048@gmail.com>

--- pages/articles/_slug.vue
+++ pages/articles/_slug.vue
@@ -1,24 +1,48 @@
 <template>
-  <v-card outlined class="post-detail-card">
-    <v-card-title>
-      <v-btn icon @click="$router.back()">
-        <v-icon>mdi-arrow-left</v-icon>
-      </v-btn>
-      <div class="width-10px" />
-      {{ page && page.title }}
-    </v-card-title>
-    <v-card-subtitle>
-      <div class="post-times">
-        <div>发布时间：{{ createTime }}</div>
-        <div class="width-10px" />
-        <div>更新时间：{{ updateTime }}</div>
+  <div>
+    <v-card v-if="!updateTime" outlined class="post-loading-card">
+      <div class="post-loading-card-header">
+        <v-skeleton-loader type="avatar" />
+        <div class="post-loading-card-header-texts">
+          <v-skeleton-loader type="heading" />
+          <v-skeleton-loader type="text" class="text-loading" />
+        </div>
       </div>
-    </v-card-subtitle>
-    <v-divider />
-    <v-card-text>
-      <nuxt-content :document="page" />
-    </v-card-text>
-  </v-card>
+      <v-divider />
+      <v-card-text>
+        <v-skeleton-loader type="text" />
+        <v-skeleton-loader type="text" />
+        <v-skeleton-loader type="text" max-width="50%" />
+        <v-skeleton-loader type="text" />
+        <v-skeleton-loader type="text" />
+        <v-skeleton-loader type="text" max-width="50%" />
+      </v-card-text>
+    </v-card>
+    <v-card v-else outlined class="post-detail-card">
+      <div class="post-detail-card-header">
+        <div class="width-10px" />
+        <v-btn icon @click="$router.back()">
+          <v-icon>mdi-arrow-left</v-icon>
+        </v-btn>
+        <div>
+          <v-card-title>
+            {{ page && page.title }}
+          </v-card-title>
+          <v-card-subtitle>
+            <div class="post-times">
+              <div>发布时间：{{ createTime }}</div>
+              <div class="width-10px" />
+              <div>更新时间：{{ updateTime }}</div>
+            </div>
+          </v-card-subtitle>
+        </div>
+      </div>
+      <v-divider />
+      <v-card-text>
+        <nuxt-content :document="page" />
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
@@ -33,13 +57,13 @@ export default Vue.extend({
   },
   computed: {
     createTime (): string {
-      if (this.page) {
+      if (this.page.createdAt) {
         return moment(this.page.createdAt).format('YYYY 年 MM 月 D 日 HH:mm:ss')
       }
       return ''
     },
     updateTime (): string {
-      if (this.page) {
+      if (this.page.updatedAt) {
         return moment(this.page.updatedAt).format('YYYY 年 MM 月 D 日 HH:mm:ss')
       }
       return ''
@@ -52,16 +76,32 @@ export default Vue.extend({
     async getData () {
       const page = await this.$content('articles', this.$route.params.slug).fetch()
       if (!Array.isArray(page)) {
-        this.page = page
+        setTimeout(() => {
+          this.page = page
+        }, 1500)
       }
     }
   }
 })
 </script>
-<style lang="less">
+<style lang="less" scoped>
 .post-times {
   display: flex;
   flex-direction: row;
+}
+.post-loading-card {
+  &-header {
+    padding: 16px;
+    display: flex;
+    align-items: center;
+    &-texts {
+      flex: 1;
+      margin-left: 16px;
+    }
+  }
+}
+.text-loading {
+  margin-top: 10px;
 }
 @media screen and (max-width: 600px){
   .post-times {
