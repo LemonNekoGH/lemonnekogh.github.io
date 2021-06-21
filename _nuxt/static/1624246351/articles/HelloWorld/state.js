54795868cc397c49443a9e866f8a2db1fd9f87ab
LemonNekoGH <LemonNekoGH@users.noreply.github.com>
2021-06-21 03:32:33 +0000

    deploy: 9533509dad9f854f39fac2542a1d7eb788f728d7

---
+++ _nuxt/static/1624246351/articles/HelloWorld/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624246351",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002FHelloWorld",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3dc46095"}}}}(null));
