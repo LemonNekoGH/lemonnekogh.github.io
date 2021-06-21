9533509dad9f854f39fac2542a1d7eb788f728d7
LemonNeko <chheese048@gmail.com>
2021-06-21 11:24:52 +0800

    :bug: extra css file to single files
    Signed-off-by: LemonNeko <chheese048@gmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -58,6 +58,7 @@ export default {
   },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extractCSS: true
   },
   generate: {
     async routes (): Promise<string[]> {
