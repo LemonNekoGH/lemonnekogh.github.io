1c222765fb510b470824be524d23a46fb04bf3f7
LemonNekoGH <LemonNekoGH@users.noreply.github.com>
2021-06-21 06:52:15 +0000

    deploy: cf621b350f0fc7d6d3f0b2885eb9d74a8fb8b709

--- _nuxt/static/1624246351/articles/setup-edk2/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624246351",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fsetup-edk2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3dc46095"}}}}(null));
---
+++ _nuxt/static/1624258334/articles/summary-of-a-week-0/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/articles/summary-of-a-week-0", {data:[{}],fetch:{},mutations:void 0});
