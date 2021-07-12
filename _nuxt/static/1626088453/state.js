2aa7ca4fa2f1aa2b4342c27e3dd19fb0564a95e8
LemonNekoGH <LemonNekoGH@users.noreply.github.com>
2021-07-12 11:14:15 +0000

    deploy: 8bd0e3b320a1e7f3bfe59d9cc01605d367f4292b

---
+++ _nuxt/static/1626088453/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626088453",layout:"default",error:a,state:{tags:[],tagColorMap:{},posts:[]},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f96daebe"}}}}(null,"\u002F"));
