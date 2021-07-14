22381e64afdeace82fe1b1c536f7a75f284cb4ea
LemonNekoGH <LemonNekoGH@users.noreply.github.com>
2021-07-14 07:49:14 +0000

    deploy: 884f515a100ce2db54cec268409ae994b26f7381

--- _nuxt/static/1626148746/articles/summary-of-a-week-0/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626148746",layout:"default",error:a,state:{tags:[],tagColorMap:{},posts:[]},serverRendered:true,routePath:"\u002Farticles\u002Fsummary-of-a-week-0",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"69991b23"}}}}(null));
--- _nuxt/static/1626148746/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626148746",layout:"default",error:a,state:{tags:[],tagColorMap:{},posts:[]},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"69991b23"}}}}(null,"\u002F"));
