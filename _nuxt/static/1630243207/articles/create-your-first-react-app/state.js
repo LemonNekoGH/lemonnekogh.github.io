cd1d16c32b1390a2037e20f167efd09489376648
LemonNekoGH <LemonNekoGH@users.noreply.github.com>
2021-08-30 10:23:15 +0000

    deploy: a66131d300e4e29d63fdcd8134385dfdd579b7e9

--- _nuxt/static/1630243207/articles/create-your-first-react-app/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630243207",layout:"default",error:a,state:{tags:[],tagColorMap:{},posts:[],categories:[]},serverRendered:true,routePath:"\u002Farticles\u002Fcreate-your-first-react-app",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d0f15da0"}}}}(null));
---
+++ _nuxt/static/1630318993/articles/summary-of-a-week-0/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630318993",layout:"default",error:a,state:{tags:[],tagColorMap:{},posts:[],categories:[]},serverRendered:true,routePath:"\u002Farticles\u002Fsummary-of-a-week-0",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4d272fdb"}}}}(null));
