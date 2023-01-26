52f7721136f384d59e0ca9d0f633d1f9718f166f
LemonNekoGH <LemonNekoGH@users.noreply.github.com>
2023-01-26 01:37:32 +0000

    deploy: 8402fad40de65589f59a5b9b0fca2b3df6991c82

---
+++ _nuxt/static/1674697050/friends/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1674697050",layout:"default",error:a,state:{tags:[],tagColorMap:{},posts:[],categories:[]},serverRendered:true,routePath:"\u002Ffriends",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"154384ed"}}}}(null));
---
+++ _nuxt/static/1674697050/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1674697050",layout:"default",error:a,state:{tags:[],tagColorMap:{},posts:[],categories:[]},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"154384ed"}}}}(null,"\u002F"));
